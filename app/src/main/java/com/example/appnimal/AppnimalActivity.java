842e4627c341a685923abb9bccee15c4941cbb3f
Stimo1026 <sandor.pepe2000@gmail.com>
2021-11-01 16:07:19 +0100

    register works

---
+++ app/src/main/java/com/example/appnimal/AppnimalActivity.java
@@ -0,0 +1,29 @@
+package com.example.appnimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AppnimalActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appnimal_main);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            Log.d(LOG_TAG, "Authenticated user!");
+        } else {
+            Log.d(LOG_TAG, "Unauthenticated user!");
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/example/appnimal/RegisterActivity.java
+++ app/src/main/java/com/example/appnimal/RegisterActivity.java
@@ -1,19 +1,77 @@
 package com.example.appnimal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText passwordConfirmEditText;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        userNameEditText = findViewById(R.id.userNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordConfirmEditText = findViewById(R.id.passwordAgainEditText);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    private void startAppnimal() {
+        Intent intent = new Intent(this, AppnimalActivity.class);
+        startActivity(intent);
     }
 
     public void cancel(View view){
         finish();
     }
+
+    public void register(View view){
+
+        String username = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String pw = passwordEditText.getText().toString();
+        String pwConfirm = passwordConfirmEditText.getText().toString();
+
+        if (!pw.equals(pwConfirm)) {
+            Log.e(LOG_TAG, "Passwords do not match");
+        }
+
+        auth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(LOG_TAG, "User created successfully");
+                    startAppnimal();
+                } else {
+                    Log.d(LOG_TAG, "User was't created successfully:", task.getException());
+                    Toast.makeText(RegisterActivity.this, "User was't created successfully:", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
+
