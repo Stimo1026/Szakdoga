d8b34782f2528b475b64fbf0ee55fe7f4f3862f8
Sándor Péter <h985628@stud.u-szeged.hu>
2021-12-10 13:24:46 +0100

    activities generated menu opens them

--- app/src/main/java/com/example/appnimal/CalendarActivity.java
+++ app/src/main/java/com/example/appnimal/CalendarActivity.java
@@ -1,50 +1,37 @@
 package com.example.appnimal;
 
-import androidx.activity.OnBackPressedDispatcherOwner;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class AppnimalActivity extends AppCompatActivity {
-    private static final String LOG_TAG = RegisterActivity.class.getName();
-    private FirebaseUser user;
-    private FirebaseAuth auth;
+public class CalendarActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
-
+    //private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appnimal_main);
-
+        setContentView(R.layout.activity_calendar);
+        /*
         navigationView = findViewById(R.id.nav_view);
         drawerLayout = findViewById(R.id.draw_layout);
         toolbar = findViewById(R.id.toolbar);
 
-        auth = FirebaseAuth.getInstance();
+        // auth = FirebaseAuth.getInstance();
 
         setSupportActionBar(toolbar);
 
@@ -54,22 +41,13 @@ public class AppnimalActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        setStatusBarcolor();
-
-        user = FirebaseAuth.getInstance().getCurrentUser();
 
         navigationView.setNavigationItemSelectedListener(this::onOptionsItemSelected);
 
-        navigationView.setCheckedItem(R.id.nav_home);
-
-        if (user != null) {
-            Log.d(LOG_TAG, "Authenticated user!");
-        } else {
-            Log.d(LOG_TAG, "Unauthenticated user!");
-            finish();
-        }
+        navigationView.setCheckedItem(R.id.nav_calendar);*/
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -82,10 +60,10 @@ public class AppnimalActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.nav_home:
+                openHome();
                 break;
 
             case R.id.nav_calendar:
-                openCalendar();
                 break;
 
             case R.id.nav_walk:
@@ -122,9 +100,14 @@ public class AppnimalActivity extends AppCompatActivity {
     }
 
     private void signOut() {
-        auth.signOut();
+        //auth.signOut();
         finish();
         Toast.makeText(this, "Log out succesfull!", Toast.LENGTH_LONG).show();
+    }
+
+    public void openHome(){
+        Intent intent = new Intent(this, AppnimalActivity.class);
+        startActivity(intent);
     }
 
     public void openCalendar() {
@@ -149,14 +132,5 @@ public class AppnimalActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
 
-    }
-
-    private void setStatusBarcolor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.transparent, this.getTheme()));
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.transparent));
-        }
-    }
-
+    }*/
 }
---
+++ app/src/main/java/com/example/appnimal/WalksActivity.java
@@ -0,0 +1,14 @@
+package com.example.appnimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WalksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walks);
+    }
+}
