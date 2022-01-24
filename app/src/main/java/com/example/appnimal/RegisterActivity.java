3a8a9476fbd8ccb259ff228d5ab80ad9656eb690
Sándor Péter <h985628@stud.u-szeged.hu>
2022-01-24 12:23:09 +0100

    database started

--- app/src/main/java/com/example/appnimal/RegisterActivity.java
+++ app/src/main/java/com/example/appnimal/RegisterActivity.java
@@ -14,6 +14,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -58,12 +60,16 @@ public class RegisterActivity extends AppCompatActivity {
         String pwConfirm = passwordConfirmEditText.getText().toString();
 
         if (!pw.equals(pwConfirm)) {
-            if (mToast != null) {mToast.cancel();}
-            mToast = Toast.makeText(RegisterActivity.this, "Password do not match!", Toast.LENGTH_LONG);
+            if (mToast != null) {
+                mToast.cancel();
+            }
+            mToast = Toast.makeText(RegisterActivity.this, "Passwords do not match!", Toast.LENGTH_LONG);
             mToast.show();
         }
         if (username.equals("") || email.equals("") || pw.equals("") || pwConfirm.equals("")) {
-            if (mToast != null) {mToast.cancel();}
+            if (mToast != null) {
+                mToast.cancel();
+            }
             mToast = Toast.makeText(RegisterActivity.this, "Something is missing!", Toast.LENGTH_LONG);
             mToast.show();
         } else {
@@ -71,17 +77,28 @@ public class RegisterActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
+                        storeNewUser();
                         //Log.d(LOG_TAG, "User created successfully");
                         startAppnimal();
                     } else {
                         //Log.d(LOG_TAG, "User was't created successfully:", task.getException());
-                        if (mToast != null) {mToast.cancel();}
+                        if (mToast != null) {
+                            mToast.cancel();
+                        }
                         mToast = Toast.makeText(RegisterActivity.this, "Error: creating user", Toast.LENGTH_LONG);
                         mToast.show();
                     }
                 }
             });
         }
+    }
+
+    private void storeNewUser() {
+        FirebaseDatabase rootNode = FirebaseDatabase.getInstance();
+        DatabaseReference reference = rootNode.getReference("User");
+
+        reference.setValue("First record!");
+
     }
 }
 
